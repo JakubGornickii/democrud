1531823ec8cfbcf17190abda1e45f13bded2bbfe
Jakub <Blade123164@gmail.com>
2018-08-20 14:28:33 +0100

    2018-08-20

--- src/main/java/pl/akademiakodu/service/EmailGeneratorServiceImpl.java
+++ src/main/java/pl/akademiakodu/service/EmailGeneratorServiceImpl.java
@@ -7,26 +7,26 @@ public class EmailGeneratorServiceImpl implements EmailGeneratorService {
 
     @Override
     public String getEmailRegistation(String token) {
-        return "W celu dokończenia rejestracji kliknij poniższy link http://localhost:8084/registration/confirm/"+token;
+        return "W celu dokończenia rejestracji kliknij poniższy link https://young-bastion-29840.herokuapp.com/registration/confirm/"+token;
     }
 
     @Override
     public String getEmailChangePass(String token) {
-        return "W celu zatwierdzenia nowego hasła http://localhost:8084/password/change/"+token;
+        return "W celu zatwierdzenia nowego hasła https://young-bastion-29840.herokuapp.com/password/change/"+token;
     }
 
     @Override
     public String getEmailChangeEmail(String token) {
-        return "W celu zmiany emaila kliknij poniższy link http://localhost:8084/email/change/"+token;
+        return "W celu zmiany emaila kliknij poniższy link https://young-bastion-29840.herokuapp.com/email/change/"+token;
     }
 
     @Override
     public String getNewEmailRegistration(String token) {
-        return "Oto nowy link do aktywacji konta http://localhost:8084/registration/confirm/"+token;
+        return "Oto nowy link do aktywacji konta https://young-bastion-29840.herokuapp.com/registration/confirm/"+token;
     }
 
     @Override
     public String getEmailPassworldRecover(String token) {
-        return "Link do nadania nowego hasła  http://localhost:8084/password/recover/"+token;
+        return "Link do nadania nowego hasła  https://young-bastion-29840.herokuapp.com/password/recover/"+token;
     }
 }
